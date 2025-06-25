cbfaa4060a2139bbf8581eb6a53fad004ce2ec0a
Hrishabh3829 <2200032748@kluniversity.in>
2025-06-25 15:06:04 +0530

    db connection added

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose"
 
 const connectDb=async ()=>{
     try {
-        await mongoose.connect(process.env.MONGODB_URL)
+        await mongoose.connect(process.env.MONGODB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
         console.log("MongoDB connected")
     } catch (error) {
         console.log(error)
