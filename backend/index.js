c6c529130e10b318036fcb5fade926751d585bdf
Hrishabh3829 <2200032748@kluniversity.in>
2025-06-25 14:50:54 +0530

    Gemini Api integrated Chat bot

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+import express from "express"
+import dotenv from "dotenv"
+import connectDb from "./config/db.js"
+import authRouter from "./routes/auth.routes.js"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+import userRouter from "./routes/user.routes.js"
+import geminiResponse from "./gemini.js"
+dotenv.config({})
+
+
+const app=express()
+
+//middleware
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
+const port=process.env.PORT
+app.use(express.json())
+app.use(cookieParser())
+
+
+//api's
+app.use("/api/auth",authRouter)
+app.use("/api/user",userRouter)
+
+
+app.listen(port,()=>{
+    connectDb()
+    console.log(`server started at port ${port}`)
+})
+
