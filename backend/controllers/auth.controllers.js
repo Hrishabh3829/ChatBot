af0543aa8896f31af5edb8ef2929b05d3c6bcc88
Hrishabh3829 <2200032748@kluniversity.in>
2025-06-25 15:33:31 +0530

    cors changed

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -24,8 +24,8 @@ try {
     res.cookie("token",token,{
         httpOnly:true,
        maxAge:7*24*60*60*1000,
-       sameSite:"strict",
-       secure:false
+       sameSite:"None",
+       secure:true
     })
 
     return res.status(201).json({
@@ -57,8 +57,8 @@ try {
     res.cookie("token",token,{
         httpOnly:true,
        maxAge:7*24*60*60*1000,
-       sameSite:"strict",
-       secure:false
+       sameSite:"None",
+       secure:true
     })
 
     const hasAssistantImage = user.assistantImage && user.assistantImage !== "default-image-url";
--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const app=express()
 
 //middleware
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://chatbot-frontend-6qhx.onrender.com",
     credentials:true
 }))
 const port=process.env.PORT
