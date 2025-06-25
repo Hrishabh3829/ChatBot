9d74bab2de1f4b9f544ad0f7e191bae0e0a9ea10
Hrishabh3829 <2200032748@kluniversity.in>
2025-06-25 15:33:00 +0530

    deploy changes

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -4,7 +4,7 @@ import { toast } from 'sonner';
 
 export const userDataContext = createContext()
 function UserContext({ children }) {
-  const serverUrl = "http://localhost:3000"
+  const serverUrl = "https://chatbot-backend-utvu.onrender.com"
   const [userData, setUserData] = useState(null)
   const [frontendImage, setFrontendImage] = useState(null)
   const [backendImage, setBackendImage] = useState(null)
